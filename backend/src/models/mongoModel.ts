ff0626bbd7f7426c9bfcac0820ac466716b87f52
Jackson <jviana116@gmail.com>
2023-03-15 16:44:03 -0300

    feat: criando model em ts

---
+++ backend/src/models/mongoModel.ts
@@ -0,0 +1,10 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+export default abstract class MongoModel<T> implements IModel<T> {
+  constructor(private model: Model<T>) {}
+
+  public async create(obj:T):Promise<T> {
+    return this.model.create({ ...obj });
+  }
+}
