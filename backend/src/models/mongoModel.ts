3d1e24aa46409a3b6823b98b9adfa87a2571217c
Jackson <jviana116@gmail.com>
2023-03-15 23:16:25 -0300

    fix: rota get

--- backend/src/models/mongoModel.ts
+++ backend/src/models/mongoModel.ts
@@ -1,14 +1,14 @@
-import { Model } from 'mongoose';
-import { IModel } from '../interfaces/IModel';
+import { Model } from "mongoose";
+import { IModel } from "../interfaces/IModel";
 
 export default abstract class MongoModel<T> implements IModel<T> {
   constructor(private model: Model<T>) {}
 
-  public async create(obj:T):Promise<T> {
+  public async create(obj: T): Promise<T> {
     return this.model.create({ ...obj });
   }
 
-  public async read():Promise<T[]> {
+  public async read(): Promise<T[]> {
     return this.model.find();
   }
 }
