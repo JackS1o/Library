c375d4380890e473ebdae84e9c476349ad230695
Jackson <jviana116@gmail.com>
2023-03-16 06:30:40 -0300

    feat: adicionando css ao cabeçalho da tela Table

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from "react";
 import booksRequest from "../api/booksRequest";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, createTheme, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { ThemeProvider } from "@emotion/react";
 
 interface IBook {
   author: string;
@@ -12,6 +23,22 @@ interface IBook {
   year: number;
   _id: string;
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+  },
+});
+
+const buttonTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#60378a",
+    },
+  },
+});
 
 function TablePage() {
   const [data, setdata] = useState<IBook[]>([]);
@@ -30,7 +57,7 @@ function TablePage() {
   const inputSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setSearch(value);
-  }
+  };
 
   const searchBook = () => {
     const filteredBooks = data.filter((book) => {
@@ -41,26 +68,52 @@ function TablePage() {
       );
     });
     setBooks(filteredBooks);
+    setSearch("");
   };
 
   return (
     <div>
-      <header>
-        <h1>Beon</h1>
-        <label>
-          Search
-          <input
-            type="text"
-            placeholder="Busque livros pelo título, autor ou idioma"
-            value={search}
-            onChange={(event) => inputSearch(event)}
-          />
-        </label>
-        <button
-          type="button"
-          onClick={searchBook}
-        >Buscar</button>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: "#d9d9d9" }}>
+          <Toolbar>
+            <Typography variant="h4" sx={{ color: "#60378a" }}>
+              Beon
+            </Typography>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 5, width: "80ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <ThemeProvider theme={buttonTheme}>
+                <TextField
+                  id="standard-textarea"
+                  label="Busque livros pelo título, autor ou idioma"
+                  placeholder="Busque livros pelo título, autor ou idioma"
+                  variant="standard"
+                  color="primary"
+                  value={search}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    inputSearch(event)
+                  }
+                  sx={{
+                    backgroundColor: "#fff",
+                    color: "#fff",
+                    borderRadius: "5px",
+                  }}
+                />
+              </ThemeProvider>
+            </Box>
+            <ThemeProvider theme={buttonTheme}>
+              <Button type="button" variant="contained" onClick={searchBook}>
+                Buscar
+              </Button>
+            </ThemeProvider>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <main>
         <table>
           <thead>
