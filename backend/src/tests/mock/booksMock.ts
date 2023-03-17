df598e9fac96071978a5b0a1f773230cf4ef416e
Jackson <jviana116@gmail.com>
2023-03-17 12:01:39 -0300

    feat: criando testes para as rotas post e get/:id

--- backend/src/tests/mock/booksMock.ts
+++ backend/src/tests/mock/booksMock.ts
@@ -11,4 +11,16 @@ const bookMock: IBook = {
   year: 2005
 }
 
-export default bookMock;
+const bookMockWithId: IBook & { _id:string } = {
+  _id: "5f9f1b9b9c9d440017a5c1b5",
+  author: "J.K. Rowling",
+  country: "United Kingdom",
+  imageLink: "http://books.google.com/books/content?id=OZz7DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  language: "English",
+  link: "http://books.google.com/books?id=OZz7DwAAQBAJ&dq=Harry+Potter&hl=&source=gbs_api",
+  pages: 309,
+  title: "Harry Potter and the Half-Blood Prince (Harry Potter  #6)",
+  year: 2005
+}
+
+export {bookMock, bookMockWithId};
--- backend/src/tests/unit/controllers/booksController.test.ts
+++ backend/src/tests/unit/controllers/booksController.test.ts
@@ -1,10 +1,10 @@
-import * as sinon from 'sinon';
-import chai from 'chai';
-import { Request, Response } from 'express';
-import BooksController from '../../../controllers/booksController';
-import BooksService from '../../../services/booksService';
-import BooksModel from '../../../models/booksModel';
-import bookMock from '../../mock/booksMock';
+import * as sinon from "sinon";
+import chai from "chai";
+import { Request, Response } from "express";
+import BooksController from "../../../controllers/booksController";
+import BooksService from "../../../services/booksService";
+import BooksModel from "../../../models/booksModel";
+import { bookMock, bookMockWithId } from "../../mock/booksMock";
 
 const { expect } = chai;
 
@@ -93,6 +93,86 @@ describe("Teste booksController", () => {
       await booksController.read(req, res);
 
       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    });
+
+    it("Deve retornar um array de livros", async () => {
+      sinon.stub(booksService, "read").resolves(bookMock as any);
+
+      await booksController.read(req, res);
+
+      expect((res.json as sinon.SinonStub).calledWith(bookMock)).to.be.true;
+    });
+  });
+
+  describe("Rota POST", () => {
+    const booksModel = new BooksModel();
+    const booksService = new BooksService(booksModel);
+    const booksController = new BooksController(booksService);
+
+    const req = {} as Request;
+    const res = {} as Response;
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("Deve retornar status 201", async () => {
+      sinon.stub(booksService, "create").resolves(bookMock as any);
+
+      req.body = bookMock;
+      await booksController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+    });
+
+    it("Deve retornar um livro", async () => {
+      sinon.stub(booksService, "create").resolves(bookMock as any);
+
+      req.body = bookMock;
+      await booksController.create(req, res);
+
+      expect((res.json as sinon.SinonStub).calledWith(bookMock)).to.be.true;
+    });
+  });
+
+  describe("Rota GET/:id", () => {
+    const booksModel = new BooksModel();
+    const booksService = new BooksService(booksModel);
+    const booksController = new BooksController(booksService);
+
+    const req = {} as Request;
+    const res = {} as Response;
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("Deve retornar status 200", async () => {
+      sinon.stub(booksService, "readOne").resolves(bookMockWithId as any);
+
+      req.params = { id: bookMockWithId._id };
+      await booksController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    });
+
+    it("Deve retornar um livro", async () => {
+      sinon.stub(booksService, "readOne").resolves(bookMockWithId as any);
+
+      req.params = { id: bookMockWithId._id };
+      await booksController.readOne(req, res);
+
+      expect((res.json as sinon.SinonStub).calledWith(bookMockWithId)).to.be.true;
     });
   });
 });
