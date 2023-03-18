77e92891cc97f7ff1c63dc18532219c54670591d
Jackson <jviana116@gmail.com>
2023-03-18 01:09:43 -0300

    fix:

--- backend/src/tests/unit/controllers/booksController.test.ts
+++ backend/src/tests/unit/controllers/booksController.test.ts
@@ -8,67 +8,6 @@ import { bookMock, bookMockWithId } from "../../mock/booksMock";
 
 const { expect } = chai;
 
-// describe('Car Controller', () => {
-//   const carModel = new CarsModel();
-//   const carService = new CarsService(carModel);
-//   const carController = new CarsController(carService);
-
-//   const req = {} as Request;
-//   const res = {} as Response;
-
-//   beforeEach(() => {
-//     res.status = sinon.stub().returns(res);
-//     res.json = sinon.stub().returns(res);
-//   });
-
-//   afterEach(() => {
-//     sinon.restore()
-//   });
-
-//   describe('Create Car', () => {
-//     beforeEach(() => {
-//       sinon.stub(carService, 'create').resolves(carMock);
-//     })
-
-//     it('Success', async () => {
-
-//       req.body = carMock;
-//       await carController.create(req, res);
-
-//       const statusStub = res.status as sinon.SinonStub;
-//       expect(statusStub.calledWith(201)).to.be.true;
-
-//       const jsonStub = res.json as sinon.SinonStub;
-//       expect(jsonStub.calledWith(carMock)).to.be.true;
-//     });
-//   });
-
-//   describe('ReadOne Car', () => {
-//     beforeEach(() => {
-//       sinon.stub(carService, 'readOne').resolves(carMock);
-//     })
-
-//     it('Success', async () => {
-//       req.params = { id: carMockWithId._id };
-//       await carController.readOne(req, res);
-
-//       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
-//       expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
-//     });
-//   });
-
-//   describe('Update Car', () => {
-//     it('Success', async () => {
-//       sinon.stub(carService, 'update').resolves(carMockWithId)
-
-//       await carController.update(req, res)
-
-//       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
-//       expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
-//     })
-//   })
-// });
-
 describe("Books Controller", () => {
   describe("Create Book", () => {
     const booksModel = new BooksModel();
